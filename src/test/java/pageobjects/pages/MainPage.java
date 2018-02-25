0ed31b1a0dc7304c757a004348320e7cfba02b7b
waldemar <waldemar.mozola@onet.pl>
2018-02-26 00:21:43 +0100

    Refactoring to pages object pattern.

--- src/test/java/pageobjects/pages/MainPage.java
+++ src/test/java/pageobjects/pages/MainPage.java
@@ -3,10 +3,22 @@ package pageobjects.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+
+
 public class MainPage {
 
     @FindBy(xpath = "//a[href='#']")
     private WebElement logOutbutton;
+
+    @FindBy(id = "search")
+    private WebElement searchField;
+
+    @FindBy (xpath = "//a[@action='push'][contains(text(),'Nasze gry')]")
+    private WebElement NaszeGrybutton;
+
+
+
+
 
     public WebElement getLogOutbutton() {
         return logOutbutton;
@@ -15,4 +27,15 @@ public class MainPage {
     public void clickOnLogOut (){
         logOutbutton.click();
     }
+
+    public void setSearchField(String text){
+        searchField.sendKeys(text);
+    }
+
+
+    public void clickOnNaszeGrybutton(){
+        NaszeGrybutton.click();
+    }
+
 }
+
--- src/test/java/pageobjects/tests/SearchFor.java
+++ src/test/java/pageobjects/tests/SearchFor.java
@@ -2,23 +2,90 @@ package pageobjects.tests;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pageobjects.pages.MainPage;
+
+import java.util.concurrent.TimeUnit;
 
 public class SearchFor {
 
 
     private WebDriver driver;
 
+    private MainPage mainPage;
+
     @Before
     public void setUp() {
 
 
-        System.setProperty("webdriver.chrome.driver", "src/test/java/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
+
+        mainPage = PageFactory.initElements(driver, MainPage.class);
+
     }
 
+    @Test
+    public void addToSearch_bb() {
+        driver.get("http://app.pinapple.jdqz1.is-academy.pl/");
+        driver.findElement(By.id("username")).sendKeys("adminps1");
+        driver.findElement(By.id("password")).sendKeys("adminps1");
+        mainPage.clickOnNaszeGrybutton();
+
+
+
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        WebElement submitNaszeGry = driver.findElement(By.cssSelector("a[href='/games-list']"));
+        submitNaszeGry.click();
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+        mainPage.setSearchField("bb");
+
+
+
+    }
+
+    @Test
+    public void addToSearchSc() {
+        driver.get("http://app.pinapple.jdqz1.is-academy.pl/");
+        driver.findElement(By.id("username")).sendKeys("adminps1");
+        driver.findElement(By.id("password")).sendKeys("adminps1");
+        WebElement submitZaloguj = driver.findElement(By.xpath("//button[text()='Zaloguj']"));
+        submitZaloguj.click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        WebElement submitNaszeGry = driver.findElement(By.cssSelector("a[href='/games-list']"));
+        submitNaszeGry.click();
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+        mainPage.setSearchField("Sc");
+    }
+    @Test
+    public void addToSearchScrrable() {
+        driver.get("http://app.pinapple.jdqz1.is-academy.pl/");
+        driver.findElement(By.id("username")).sendKeys("adminps1");
+        driver.findElement(By.id("password")).sendKeys("adminps1");
+        WebElement submitZaloguj = driver.findElement(By.xpath("//button[text()='Zaloguj']"));
+        submitZaloguj.click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        WebElement submitNaszeGry = driver.findElement(By.cssSelector("a[href='/games-list']"));
+        submitNaszeGry.click();
+        WebDriverWait wait = new WebDriverWait(driver, 1);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+        mainPage.setSearchField("Scrabble");
+    }
 
     @After
-    public void tearDown() { driver.close(); }
+
+    public void tearDown() {
+        driver.close();
+    }
 }
